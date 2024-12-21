7dcc5af803ae26911bb4a079386df7c5f4c433da
gczarny <grzegorz.czarny93@gmail.com>
2024-12-21 18:33:29 +0100

    feat: add asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import display, time
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -9,22 +11,39 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     _clock = time.Clock()
+
     dt = 0
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-    my_group = pygame.sprite.Group()
+    asteroid_field = AsteroidField()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         display.flip()
-        time_passed = _clock.tick(60)
-        dt += time_passed / 1000.0
+
+        dt += _clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
