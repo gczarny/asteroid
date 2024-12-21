23c246e72513e8fedbcc99eb8e676e63c18d6bee
gczarny <grzegorz.czarny93@gmail.com>
2024-12-21 18:19:10 +0100

    feat: rotating

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         display.flip()
         time_passed = _clock.tick(60)
