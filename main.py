17b7eeb5fc8ff2dc6ef9bde6cb5a430bbfa0f1b1
gczarny <grzegorz.czarny93@gmail.com>
2024-12-21 18:16:02 +0100

    feat: add player

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
-from pygame import display
+from pygame import display, time
 
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,12 +10,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    _clock = time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         display.flip()
+        time_passed = _clock.tick(60)
+        dt += time_passed / 1000.0
+
 
 if __name__ == "__main__":
     main()
